73324be3d7cfa7e9744fb37adf7e3e67f7075cde
Akın <aknyldrm361@gmail.com>
2023-02-21 14:31:16 +0300

    Enter Play Mode Scene changed simplified

--- Assets/EditorLoadingSceneLoad/Editor/EnterPlayModeSceneChange.cs
+++ Assets/EditorLoadingSceneLoad/Editor/EnterPlayModeSceneChange.cs
@@ -1,26 +1,15 @@
 using UnityEditor;
 using UnityEditor.SceneManagement;
-using UnityEngine;
 
 [InitializeOnLoad]
-public static class EnterPlayModeSceneChange
-{ 
+public class EnterPlayModeSceneChange
+{
     private static readonly string loadingScenePath = "Assets/EditorLoadingSceneLoad/LoadingScene.unity";
-
+    
+    private static readonly SceneAsset loadingScene = AssetDatabase.LoadAssetAtPath<SceneAsset>(loadingScenePath);
+    
     static EnterPlayModeSceneChange()
     {
-        SceneAsset loadingScene = AssetDatabase.LoadAssetAtPath<SceneAsset>(loadingScenePath);
-
-        if (loadingScene == null)
-        {
-            Debug.LogWarning("There is no LoadingScene or not found!");
-            
-            EditorSceneManager.playModeStartScene =
-                AssetDatabase.LoadAssetAtPath<SceneAsset>(EditorSceneManager.GetActiveScene().path);
-            
-            return;
-        }
-        
         EditorSceneManager.playModeStartScene = loadingScene;
     }
 }
